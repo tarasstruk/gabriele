b698c1ba7402a7e8b24d8b260d99b4e4f2630e37
Taras <t.struk@sonnen.de>
2023-12-03 19:21:56 +0100

    add daisy wheel string iterators

--- src/daisy/german.rs
+++ src/daisy/german.rs
@@ -1,27 +1,141 @@
-#[allow(unused)]
-pub const START_INDEX: u8 = 1;
+use super::{Impact, Symbol};
+
+impl Symbol {
+    #[allow(unused)]
+    fn new(idx: u8, chr: char) -> Self {
+        Self {
+            idx,
+            chr,
+            imp: Default::default(),
+        }
+    }
+
+    #[allow(unused)]
+    fn imp(mut self, impact: Impact) -> Self {
+        self.imp = impact;
+        self
+    }
+}
 
 #[allow(unused)]
-pub const MAP: [char; 100] = [
-    // 01 - 05
-    '.', ',', '-', 'v', 'l', // 06 - 10
-    'm', 'j', 'w', ' ', ' ', // 11 - 15
-    'f', '^', '>', ' ', '+', // 16 - 20
-    '1', '2', '3', '4', '5', // 21 - 25
-    '6', '7', '8', '9', '0', // 26 - 30
-    'E', '|', 'B', 'F', 'P', // 31 - 35
-    ' ', ' ', ' ', ' ', ' ', // 36 - 40
-    ' ', ' ', ' ', ' ', ' ', // 41 - 45
-    ' ', ' ', ' ', ' ', ' ', // 46 - 50
-    ' ', ' ', ' ', ' ', ' ', // 51 - 55
-    ' ', ' ', ' ', ' ', ' ', // 56 - 60
-    ' ', ' ', ' ', ' ', ' ', // 61 - 65
-    ' ', ' ', ' ', ' ', ' ', // 66 - 70
-    ' ', ' ', ' ', ' ', ' ', // 71 - 75
-    ' ', ' ', ' ', ' ', ' ', // 76 - 80
-    ' ', ' ', ' ', ' ', ' ', // 81 - 85
-    ' ', ' ', ' ', ' ', ' ', // 86 - 90
-    ' ', ' ', ' ', ' ', ' ', // 91 - 95
-    ' ', ' ', ' ', ' ', ' ', // 96 - 100
-    ' ', ' ', ' ', ' ', ' ',
-];
+pub struct Db {
+    symbols: [Symbol; 100],
+    unknown: Symbol,
+}
+
+impl Db {
+    pub fn new() -> Self {
+        let symbols = [
+            Symbol::new(1, '.').imp(Impact::Soft),
+            Symbol::new(2, ',').imp(Impact::Soft),
+            Symbol::new(3, '-').imp(Impact::Soft),
+            Symbol::new(4, 'v'),
+            Symbol::new(5, 'l'),
+            Symbol::new(6, 'm'),
+            Symbol::new(7, 'j'),
+            Symbol::new(8, 'w'),
+            Symbol::new(9, '²'), // U+00B2
+            Symbol::new(10, 'µ'),
+            Symbol::new(11, 'f'),
+            Symbol::new(12, '^'),
+            Symbol::new(13, '>'),
+            Symbol::new(14, '´'),
+            Symbol::new(15, '+'),
+            Symbol::new(16, '1'),
+            Symbol::new(17, '2'),
+            Symbol::new(18, '3'),
+            Symbol::new(19, '4'),
+            Symbol::new(20, '5'),
+            Symbol::new(21, '6'),
+            Symbol::new(22, '7'),
+            Symbol::new(23, '8'),
+            Symbol::new(24, '9'),
+            Symbol::new(25, '0'),
+            Symbol::new(26, 'E'),
+            Symbol::new(27, '|'),
+            Symbol::new(28, 'B'),
+            Symbol::new(29, 'F'),
+            Symbol::new(30, 'P'),
+            Symbol::new(31, 'S'),
+            Symbol::new(32, 'Z'),
+            Symbol::new(33, 'V'),
+            Symbol::new(34, '&'),
+            Symbol::new(35, 'Y'),
+            Symbol::new(36, 'A'),
+            Symbol::new(37, 'T'),
+            Symbol::new(38, 'L'),
+            Symbol::new(39, '$'),
+            Symbol::new(40, 'R'),
+            Symbol::new(41, '*'),
+            Symbol::new(42, 'C'),
+            Symbol::new(43, '"'),
+            Symbol::new(44, 'D'),
+            Symbol::new(45, '?'),
+            Symbol::new(46, 'N'),
+            Symbol::new(47, 'I'),
+            Symbol::new(48, 'U'),
+            Symbol::new(49, ')'),
+            Symbol::new(50, 'W').imp(Impact::Hard),
+            Symbol::new(51, '_'),
+            Symbol::new(52, '='),
+            Symbol::new(53, ';'),
+            Symbol::new(54, ':'),
+            Symbol::new(55, 'M').imp(Impact::Hard),
+            Symbol::new(56, '\''),
+            Symbol::new(57, 'H'),
+            Symbol::new(58, '('),
+            Symbol::new(59, 'K'),
+            Symbol::new(60, '/'),
+            Symbol::new(61, 'O').imp(Impact::Hard),
+            Symbol::new(62, '!'),
+            Symbol::new(63, 'X'),
+            Symbol::new(64, '§').imp(Impact::Hard),
+            Symbol::new(65, 'Q').imp(Impact::Hard),
+            Symbol::new(66, 'J'),
+            Symbol::new(67, '%'),
+            Symbol::new(68, '³'), // U+00B3
+            Symbol::new(69, 'G'),
+            Symbol::new(70, '°'),
+            Symbol::new(71, 'Ü').imp(Impact::Hard),
+            Symbol::new(72, '`').imp(Impact::Soft),
+            Symbol::new(73, 'Ö'),
+            Symbol::new(74, '<'),
+            Symbol::new(75, 'Ä').imp(Impact::Hard),
+            Symbol::new(76, '#'),
+            Symbol::new(77, 't'),
+            Symbol::new(78, 'x'),
+            Symbol::new(79, 'q'),
+            Symbol::new(80, 'ß'),
+            Symbol::new(81, 'ü'),
+            Symbol::new(82, 'ö'),
+            Symbol::new(83, 'ä'),
+            Symbol::new(84, 'y'),
+            Symbol::new(85, 'k'),
+            Symbol::new(86, 'p'),
+            Symbol::new(87, 'h'),
+            Symbol::new(88, 'c'),
+            Symbol::new(89, 'g'),
+            Symbol::new(90, 'n'),
+            Symbol::new(91, 'r'),
+            Symbol::new(92, 's'),
+            Symbol::new(93, 'e'),
+            Symbol::new(94, 'a'),
+            Symbol::new(95, 'i'),
+            Symbol::new(96, 'd'),
+            Symbol::new(97, 'u'),
+            Symbol::new(98, 'b'),
+            Symbol::new(99, 'o'),
+            Symbol::new(100, 'z'),
+        ];
+        let unknown = Symbol::new(41, '*');
+        Self { symbols, unknown }
+    }
+
+    #[allow(unused)]
+    pub fn get(&self, character: char) -> &Symbol {
+        if let Some(result) = self.symbols.iter().find(|symbol| symbol.chr == character) {
+            return result;
+        }
+        &(self.unknown)
+    }
+}
--- src/daisy/mod.rs
+++ src/daisy/mod.rs
@@ -1,1 +1,66 @@
 mod german;
+use std::default::Default;
+
+#[derive(Default)]
+#[repr(u16)]
+#[allow(unused)]
+pub enum Impact {
+    #[default]
+    Middle = 127,
+    Hard = 255,
+    Soft = 90,
+    Custom(u16),
+}
+
+pub struct Symbol {
+    pub idx: u8,
+    pub imp: Impact,
+    pub chr: char,
+}
+
+trait Printable {
+    fn index(&self) -> u8;
+}
+
+impl Printable for Symbol {
+    fn index(&self) -> u8 {
+        self.idx
+    }
+}
+
+#[allow(unused)]
+pub fn database() -> german::Db {
+    german::Db::new()
+}
+
+#[allow(unused)]
+fn get_symbols<'a>(input: &'a str, db: &'a german::Db) -> impl Iterator<Item = u8> + 'a {
+    input
+        .chars()
+        .enumerate()
+        .map(move |(_, chr)| db.get(chr).index())
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::daisy::*;
+
+    #[test]
+    fn test_string_to_iterator_over_symbols() {
+        let db = database();
+        let mut first_iterator = get_symbols("Wombat", &db);
+
+        let mut second_iterator = get_symbols("Wombat", &db);
+        let value = first_iterator.next();
+        assert_eq!(value, Some(50_u8));
+
+        let value = first_iterator.next();
+        assert_eq!(value, Some(99_u8));
+
+        let value = second_iterator.next();
+        assert_eq!(value, Some(50_u8));
+
+        let value = first_iterator.next();
+        assert_eq!(value, Some(6_u8));
+    }
+}
