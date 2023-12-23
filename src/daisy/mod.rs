c44f912484023383520c7c7ab257a09af899ad17
Taras <t.struk@sonnen.de>
2023-12-23 14:02:21 +0100

    introduce AfterSymbolPrinted

--- src/daisy/mod.rs
+++ src/daisy/mod.rs
@@ -1,4 +1,5 @@
 pub mod german;
+use crate::gabi::printing::Instruction;
 use std::default::Default;
 
 #[derive(Default)]
@@ -30,12 +31,12 @@ pub enum Impression {
 
 impl Impression {
     #[allow(unused)]
-    fn convert_value(ratio: f32) -> u16 {
-        (ratio * 63.0) as u16
+    fn convert_value(ratio: f32) -> u8 {
+        (ratio * 63.0) as u8
     }
 
     #[allow(unused)]
-    fn value(&self) -> u16 {
+    pub fn value(&self) -> u8 {
         match self {
             Self::Custom(ratio) => Self::convert_value(*ratio),
             Self::Strongest => Self::convert_value(1.0),
@@ -55,11 +56,36 @@ pub enum ActionMapping {
 }
 
 #[derive(PartialEq, Debug, Clone, Default)]
+pub enum AfterSymbolPrinted {
+    // sets bits "7"=0 and "6"=0
+    #[default]
+    MoveRight,
+    // sets bits "7"=1 and "6"=0
+    #[allow(unused)]
+    MoveLeft,
+    // sets bits "7"=0 and "6"=1
+    #[allow(unused)]
+    HoldOn,
+}
+
+impl AfterSymbolPrinted {
+    #[allow(unused)]
+    pub fn value(&self) -> u8 {
+        match self {
+            Self::MoveRight => 0,
+            Self::MoveLeft => 0b1000_0000,
+            Self::HoldOn => 0b0100_0000,
+        }
+    }
+}
+
+#[derive(PartialEq, Debug, Clone, Default)]
 pub struct Symbol {
     pub idx: u8,
     pub imp: Impression,
     pub chr: char,
     pub act: ActionMapping,
+    pub after: AfterSymbolPrinted,
 }
 
 impl Symbol {
@@ -78,6 +104,7 @@ impl Symbol {
             chr: ' ',
             imp: Default::default(),
             act: ActionMapping::Whitespace,
+            after: Default::default(),
         }
     }
 
@@ -87,12 +114,19 @@ impl Symbol {
             chr: '\n',
             imp: Default::default(),
             act: ActionMapping::CarriageReturn,
+            after: Default::default(),
         }
     }
 
     #[allow(unused)]
     pub fn imp(mut self, impact: Impression) -> Self {
         self.imp = impact;
+        self
+    }
+
+    #[allow(unused)]
+    pub fn after_printed(mut self, after: AfterSymbolPrinted) -> Self {
+        self.after = after;
         self
     }
 
@@ -105,11 +139,29 @@ impl Symbol {
     pub fn strong(mut self) -> Self {
         self.imp(Impression::Strong)
     }
+
+    #[allow(unused)]
+    pub fn hold(mut self) -> Self {
+        self.after_printed(AfterSymbolPrinted::HoldOn)
+    }
+
+    #[allow(unused)]
+    pub fn left(mut self) -> Self {
+        self.after_printed(AfterSymbolPrinted::MoveLeft)
+    }
+
+    #[allow(unused)]
+    pub fn instructions(&self) -> Box<dyn Iterator<Item = Instruction>> {
+        let b1 = self.idx;
+        let b2 = self.imp.value() | self.after.value();
+        Box::new([Instruction::bytes(b1, b2)].into_iter())
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::gabi::printing::Instruction;
 
     #[test]
     fn test_strong_impression() {
@@ -129,5 +181,28 @@ mod tests {
     #[test]
     fn test_custom_impression() {
         assert_eq!(Impression::Custom(0.8).value(), 50)
+    }
+
+    #[test]
+    fn test_instructions_with_strong_impression() {
+        let symbol = Symbol::new(81, 'ü').strong();
+        let mut result = symbol.instructions();
+        assert_eq!(result.next(), Some(Instruction::bytes(81, 47)));
+        assert_eq!(result.next(), None);
+    }
+
+    #[test]
+    fn test_instructions_with_hold_after_printed() {
+        let symbol = Symbol::new(81, 'ü').hold();
+        let mut result = symbol.instructions();
+        assert_eq!(result.next(), Some(Instruction::bytes(81, 31 + 64)));
+        assert_eq!(result.next(), None);
+    }
+    #[test]
+    fn test_instructions_with_left_direction() {
+        let symbol = Symbol::new(81, 'ü').left();
+        let mut result = symbol.instructions();
+        assert_eq!(result.next(), Some(Instruction::bytes(81, 31 + 128)));
+        assert_eq!(result.next(), None);
     }
 }
