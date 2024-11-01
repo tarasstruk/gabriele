2e741503005720a8be4461f407ef29b06c648039
Taras Struk <t.struk@sonnen.de>
2024-11-01 16:53:10 +0100

    integration test with spawning a blocking thread, resembling the usage for the non-async serialport crate

--- tests/test_app.rs
+++ tests/test_app.rs
@@ -3,9 +3,11 @@ use gabriele::machine::Machine;
 use gabriele::printing::Instruction;
 use std::fs;
 use std::sync::{Arc, Mutex};
+use std::thread::sleep;
+use std::time::Duration;
 use tokio::sync::mpsc;
 use tokio::sync::mpsc::{UnboundedReceiver, UnboundedSender};
-use tokio::task::{spawn, JoinHandle};
+use tokio::task::JoinHandle;
 
 pub struct TestApp {
     pub latch: Arc<Mutex<Vec<Instruction>>>,
@@ -25,35 +27,38 @@ pub struct TestRunner {
 }
 
 impl TestRunner {
-    pub async fn run(&mut self, latch: Arc<Mutex<Vec<Instruction>>>) {
+    pub fn run(&mut self, latch: Arc<Mutex<Vec<Instruction>>>) {
         println!("------RUNNER HAS STARTED----");
         loop {
-            match self.receiver.recv().await {
-                Some(Instruction::Shutdown) => break,
-                Some(instruction) => {
+            match self.receiver.try_recv() {
+                Ok(Instruction::Shutdown) => break,
+                Ok(instruction) => {
                     println!("RECEIVED INSTRUCTION: {:?}", instruction);
                     let mut items = latch.lock().unwrap();
                     items.push(instruction);
                 }
-                _ => continue,
+                _ => {
+                    sleep(Duration::from_millis(2));
+                    continue;
+                }
             }
         }
     }
 }
 
-async fn start_runner(rx: UnboundedReceiver<Instruction>, latch: Arc<Mutex<Vec<Instruction>>>) {
+fn start_runner(rx: UnboundedReceiver<Instruction>, latch: Arc<Mutex<Vec<Instruction>>>) {
     let mut runner = TestRunner { receiver: rx };
-    runner.run(latch).await
+    runner.run(latch)
 }
 
-async fn start_test_app() -> (TestApp, JoinHandle<()>) {
+fn start_test_app() -> (TestApp, JoinHandle<()>) {
     let (tx, rx) = mpsc::unbounded_channel::<Instruction>();
 
     let app = TestApp::new(tx);
     let latch = app.latch.clone();
 
-    let handle = spawn(async move { start_runner(rx, latch).await });
-    (app, handle)
+    let runner_handle = tokio::task::spawn_blocking(move || start_runner(rx, latch));
+    (app, runner_handle)
 }
 
 fn load_test_db() -> Db {
@@ -63,13 +68,13 @@ fn load_test_db() -> Db {
 
 #[tokio::test]
 async fn starts_test_application() {
-    let (mut app, runner_handle) = start_test_app().await;
+    let (mut app, runner) = start_test_app();
     let db = load_test_db();
 
     app.machine.print("AT", &db);
     app.machine.shutdown();
 
-    _ = tokio::join!(runner_handle);
+    _ = tokio::join!(runner);
     let latch = app.latch.lock().unwrap();
 
     assert_eq!(latch.len(), 2);
