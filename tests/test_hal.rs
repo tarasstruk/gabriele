e899960669fc8d1a8e63e36062cf81d773acb042
Taras Struk <t.struk@sonnen.de>
2024-11-03 21:02:28 +0100

    fix integration test for handshake sequence

--- tests/test_hal.rs
+++ tests/test_hal.rs
@@ -24,18 +24,24 @@ async fn awaits_prepare_response_from_typewriter() {
     tx.send(Instruction::Prepare).unwrap();
     tx.send(Instruction::Halt).unwrap();
 
-    tokio::time::sleep(tokio::time::Duration::from_secs(3)).await;
-    let mut read_buffer = [0u8; 6];
-    port.read_exact(&mut read_buffer).unwrap();
+    tokio::time::sleep(tokio::time::Duration::from_secs(5)).await;
 
-    assert_eq!(&read_buffer, &[0xA3, 0x00, 0xA0, 0x00, 0xA1, 0x00]);
+    // expect the start handshake sequence of 8 bytes to be written by HAL
+    let mut read_buffer = [0u8; 8];
+    port.read_exact(&mut read_buffer).unwrap();
+    assert_eq!(
+        &read_buffer,
+        &[0xA3, 0x00, 0xA0, 0x00, 0xA1, 0x00, 0xA4, 0x00]
+    );
 
     port.clear(ClearBuffer::All).unwrap();
+    // write the typewriters feedback response to be read by HAL
     port.write_all(&[161_u8]).unwrap();
 
     _ = tokio::join!(handle);
 
+    // expect the end handshake 2 bytes to be written by HAL
     let mut read_buffer = [0u8; 2];
     port.read_exact(&mut read_buffer).unwrap();
-    assert_eq!(&read_buffer, &[0xA4, 0x00]);
+    assert_eq!(&read_buffer, &[0xA2, 0x00]);
 }
