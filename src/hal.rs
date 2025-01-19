046b4d242bdf1cb4b37373748db5d8533a473478
Taras Struk <t.struk@sonnen.de>
2025-01-19 13:17:12 +0100

    use ring incdicator and cts sigglal to control flow

--- src/hal.rs
+++ src/hal.rs
@@ -9,11 +9,17 @@ use tokio::sync::mpsc::UnboundedReceiver;
 pub struct Hal {
     conn: Box<dyn SerialPort>,
     receiver: UnboundedReceiver<Instruction>,
+    cts_control: bool,
 }
 
 impl Hal {
     pub fn new(conn: Box<dyn SerialPort>, receiver: UnboundedReceiver<Instruction>) -> Self {
-        Hal { conn, receiver }
+        let cts_control = true;
+        Hal {
+            conn,
+            receiver,
+            cts_control,
+        }
     }
 
     pub fn run(&mut self) {
@@ -41,7 +47,7 @@ impl Hal {
     }
 
     pub fn write_byte(&mut self, input: u8) {
-        self.reset_latch().unwrap();
+        debug!("Writing byte: {}", input);
         wait(2);
 
         self.conn
@@ -53,6 +59,24 @@ impl Hal {
             let ri = self.conn.read_ring_indicator().unwrap();
             debug!("Ring Indicator state: {:?}", ri);
             if ri {
+                if !self.cts_control {
+                    debug!("cts control is disabled");
+                    return;
+                }
+                // wait(10);
+                let mut cts_counter = 10_u32;
+                loop {
+                    wait(2);
+                    if let Ok(true) = self.conn.read_clear_to_send() {
+                        wait(2);
+                        break;
+                    }
+                    cts_counter -= 1;
+                    debug!("cts_counter: {}", cts_counter);
+                    if cts_counter == 0 {
+                        panic!("no cts signal")
+                    }
+                }
                 break;
             }
             counter -= 1;
@@ -97,7 +121,11 @@ impl Hal {
 
     fn go_offline(&mut self) {
         info!("go off-line");
-        self.command(&[0xA0, 0x00]);
+        self.write_byte(0xA0);
+        wait_short();
+        self.cts_control = false;
+        self.write_byte(0x00);
+        // self.command(&[0xA0, 0x00]);
     }
 
     fn go_online(&mut self) {
